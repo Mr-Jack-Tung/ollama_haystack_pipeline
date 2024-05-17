4b6a1ad1486958cbfe824d0b96a2a176740b80a8
Mr. Jack Tung <72214141+Mr-Jack-Tung@users.noreply.github.com>
2024-05-17 23:41:55 +0700

    Add files via upload

---
+++ ollama_haystack_pipeline.py
@@ -0,0 +1,64 @@
+
+# pip install haystack
+# pip install haystack-ai
+# pip install ollama-haystack
+
+import os
+from haystack import Pipeline, Document
+from haystack.document_stores.in_memory import InMemoryDocumentStore
+from haystack.components.retrievers.in_memory import InMemoryBM25Retriever
+# from haystack.components.generators import OpenAIGenerator
+from haystack.components.builders.answer_builder import AnswerBuilder
+from haystack.components.builders.prompt_builder import PromptBuilder
+
+
+document_store = InMemoryDocumentStore()
+document_store.write_documents([
+    Document(content="My name is Jean and I live in Paris."), 
+    Document(content="My name is Mark and I live in Berlin."), 
+    Document(content="My name is Giorgio and I live in Rome.")
+])
+
+prompt_template = """
+Given these documents, answer the question.
+Documents:
+{% for doc in documents %}
+    {{ doc.content }}
+{% endfor %}
+Question: {{question}}
+Answer:
+"""
+
+retriever = InMemoryBM25Retriever(document_store=document_store)
+prompt_builder = PromptBuilder(template=prompt_template)
+# llm = OpenAIGenerator(api_key=api_key)
+
+from haystack_integrations.components.generators.ollama import OllamaGenerator
+llm = OllamaGenerator(model="phi3",
+                            url = "http://localhost:11434/api/generate",
+                            generation_kwargs={
+                              "num_predict": 100,
+                              "temperature": 0.9,
+                              })
+
+rag_pipeline = Pipeline()
+rag_pipeline.add_component("retriever", retriever)
+rag_pipeline.add_component("prompt_builder", prompt_builder)
+rag_pipeline.add_component("llm", llm)
+rag_pipeline.connect("retriever", "prompt_builder.documents")
+rag_pipeline.connect("prompt_builder", "llm")
+
+question = "Who lives in Paris?"
+results = rag_pipeline.run(
+    {
+        "retriever": {"query": question},
+        "prompt_builder": {"question": question},
+    }
+)
+
+print(results["llm"]["replies"])
+
+"""
+Ranking by BM25...: 100%|███████████████████████████████████████████████████████████████████████████████████| 3/3 [00:00<00:00, 30615.36 docs/s]
+['Jean lives in Paris.']
+"""
